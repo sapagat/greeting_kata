4a946a9fe856bb66b255f6581de14b6bce4bfe5a
Sam Gathercole <sam@nextail.co>
2019-06-20 12:40:32 +0200

    Append test output to commit description

--- test.sh
+++ test.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+OUTPUT=/tmp/kata-tracker.txt
 
-npm test
-if [ "$?" -eq 0 ];then
+npm test | tee $OUTPUT
+if [ "${PIPESTATUS[0]}" -eq 0 ];then
   status="GREEN"
 else
   status="RED"
 fi
 
 git add -A
-git commit -m "$status $(date +%s)"
+message="$status $(date +%s)"
+description="$(cat $OUTPUT)"
+git commit -m "$message" -m "$description"
