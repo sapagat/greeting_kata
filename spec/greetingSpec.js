ce19e62b502e34618ee9ede2c1ec0dafed04a073
Sam Gathercole <sam@nextail.co>
2019-06-21 17:15:09 +0200

    RED 1561130109
    > greeting@1.0.0 test /home/sapagat/sandbox/greeting
    > jasmine --reporter=jasmine-console-reporter
    
    Executing 2 defined specs...
    Running in random order... (seed: 76823)
    
    Test Suites & Specs:
    
    1. Greeting
       ✖ says hello to the given name (1 failure) (1ms)
    
    2. Setup
       ✔ is working (1ms)
    
    >> Done!
    
    Failed Specs:
    
    1. Greeting : says hello to the given name
       ReferenceError: greet is not defined
           at <Jasmine>
           at UserContext.it /home/sapagat/sandbox/greeting/spec/greetingSpec.js:4:5
           at <Jasmine>
           at runCallback timers.js:705:18
           at tryOnImmediate timers.js:676:5
           at processImmediate timers.js:658:5
    
    Summary:
    
    ❌  Failed
    Suites:  2 of 2
    Specs:   2 of 2
    Expects: 2 (1 failure)
    Finished in 0.01 seconds

---
+++ spec/greetingSpec.js
@@ -0,0 +1,7 @@
+describe('Greeting', () => {
+  it('says hello to the given name', () => {
+    const name = "Lorena"
+    result = greet(name)
+    expect(result).toEqual("Hello, Lorena.")
+  })
+})
